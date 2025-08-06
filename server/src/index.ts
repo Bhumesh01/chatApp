f6f60b7df117b01aab1420b2115d1b7b1dd47520
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-08-06 22:25:33 +0530

    broadcasting logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello")
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({port: 8000});
+let userCount = 0;
+let allSockets = [];
+wss.on("connection", function(socket){
+    allSockets.push(socket);
+    userCount++;
+    socket.send("user Connected");
+    allSockets.forEach(element => {
+        element.send("total users: " + userCount);
+    });
+    socket.on("message", function(e){
+        allSockets.forEach((element)=>{
+            element!=socket&&element.send(`${e.toString()}`);
+        })
+    })
+});
