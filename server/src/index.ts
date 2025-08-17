737bd43dd4d0518df7b2a306afb3ee46bd4e0d92
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2025-08-17 21:53:32 +0530

    Made Backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,54 @@
 import { WebSocketServer, WebSocket } from 'ws';
 
-const wss = new WebSocketServer({port: 8000});
-let userCount = 0;
-let allSockets: WebSocket[] = [];
-wss.on("connection", function(socket){
-    allSockets.push(socket);
-    userCount++;
-    socket.send("user Connected");
-    allSockets.forEach(element => {
-        element.send("total users: " + userCount);
+interface UserRequest{
+    "type": "join"|"chat",
+    "payload": Record<"message"|"roomId", string>
+}
+
+const wss = new WebSocketServer({port: 8080});
+const arina = new Map<string, Set<WebSocket>>();
+const user = new Map<WebSocket, string>();
+wss.on("connection", function(socket:WebSocket){
+    socket.on("error", console.error);
+    socket.on("message", (e)=>{
+        let request: UserRequest;
+        try{
+            request = JSON.parse(e.toString());
+        }
+        catch{
+            socket.send(JSON.stringify({ type: "error", message: "Invalid JSON" }));
+            return;
+        }
+        // When the user wants to join the room
+        if(request.type === "join" && request.payload.roomId){
+            const roomId = request.payload.roomId;
+            if(!arina.has(roomId)){
+                arina.set(roomId, new Set());
+            }
+            arina.get(roomId)?.add(socket);
+            user.set(socket, roomId);
+            socket.send(JSON.stringify({"message": "joined successfully"}));
+        }
+        // when the user wants to chat ie send messages
+        else if(request.type === "chat" && request.payload.message){
+            const roomId = user.get(socket);
+            if(!roomId){
+                socket.send(JSON.stringify({message: "You are not in a room" }));
+                return;
+            }
+            arina.get(roomId)?.forEach(element=>element!==socket&&element.send(JSON.stringify(request)));
+            socket.send(JSON.stringify({message: "Message sent successfully" }));
+        }
     });
-    socket.on("message", function(e){
-        allSockets.forEach((element)=>{
-            element!=socket&&element.send(`${e.toString()}`);
-        })
+    // when the user leaves the room
+    socket.on("close", ()=>{
+        const roomId = user.get(socket);
+        if(roomId && arina.get(roomId)){
+            arina.get(roomId)?.delete(socket);
+            if (arina.get(roomId)?.size === 0) {
+                arina.delete(roomId);
+            }
+            user.delete(socket);
+        }
     })
 });
